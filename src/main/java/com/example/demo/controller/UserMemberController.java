369d79d88d7c03b8a46e0150abb3f6cec1c672ff
yeosang0508 <yeosang0508@gmail.com>
2024-08-15 20:45:11 +0900

    로그인 기능 구현

--- src/main/java/com/example/demo/controller/UserMemberController.java
+++ src/main/java/com/example/demo/controller/UserMemberController.java
@@ -10,11 +10,49 @@ import com.example.demo.util.Ut;
 import com.example.demo.vo.Member;
 import com.example.demo.vo.ResultData;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class UserMemberController {
 
 	@Autowired
 	private MemberService memberService;
+	
+	@RequestMapping("/user/member/doLogin")
+	@ResponseBody
+	public ResultData<Member> doLogin(HttpSession httpSession, String loginId, String loginPw){
+		boolean isLogined = false;
+		
+		if(httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+		
+		if(isLogined) {
+			return ResultData.from("F-A", "이미 로그인 함");
+		}
+		
+		if(Ut.isEmptyOrNull(loginId)) {
+			return ResultData.from("F-1", "loginId 입력 x");
+		}
+		
+		if(Ut.isEmptyOrNull(loginPw)) {
+			return ResultData.from("F-2", "loginPw 입력 x");
+		}
+		
+		Member member = memberService.getMemberByLoginId(loginId);
+		
+		if(member == null) {
+			return ResultData.from("F-3", Ut.f("%s는(은) 존재 x", loginId));
+		}
+		
+		if(member.getLoginPw().equals(loginPw) == false) {
+			return ResultData.from("F-4", Ut.f("비밀번호 틀림"));
+		}
+		
+		httpSession.setAttribute("loginedMemberId", member.getId());
+		
+		return ResultData.from("S-1", Ut.f("%s님 환영합니다.", member.getNickname()));
+	}
 
 	@RequestMapping("/user/member/doJoin")
 	@ResponseBody
--- src/main/java/com/example/demo/vo/Member.java
+++ src/main/java/com/example/demo/vo/Member.java
@@ -13,6 +13,7 @@ public class Member {
 	private String regDate;
 	private String updateDate;
 	private String loginId;
+	private String loginPw;
 	private String name;
 	private String nickname;
 	private String cellphoneNum;
